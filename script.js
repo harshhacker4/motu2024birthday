2d61c30e325b81c9866b539cfa8c528707c20d91
harshhacker4 <harshhacker4@gmail.com>
2024-03-03 22:32:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,212 @@
+const birthday_journals = {
+    "days": [
+      { "image": "https://assets-global.website-files.com/65e4b51d34d216f5a1a63fb0/65e4d5b9ac4e605ee40ff5d7_WhatsApp%20Image%202024-03-03%20at%208.30.18%20PM.jpeg",
+        "date": "February 4th, 2024",
+        "message": "One month until your birthday, Motu. 30 days to celebrate the day you graced the world with your light, your laugh, and your gaplu daplu charm. It feels like yesterday we were tangled in the early days of \"love you\" versus \"I love you,\" a silly fight compared to the mountains we've climbed together."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b857ba600e3f87bc33_WhatsApp%20Image%202024-03-03%20at%208.30.24%20PM%20(1).jpeg",
+        "date": "February 5th, 2024",
+        "message": "Your eyes, Motu. They hold universes, galaxies of emotions, and a depth I could spend a lifetime trying to understand. Those spectacles, perched just right on your nose, add a touch of mystery to your beauty, just like the question mark (?) it makes, like a hidden superpower waiting to be unleashed."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b7b3f249496972a3c4_WhatsApp%20Image%202024-03-03%20at%208.30.29%20PM%20(1).jpeg",
+        "date": "February 6th, 2024",
+        "message": "I remember the day we met, the 26th of September – a date etched in my memory with your name. We've navigated ups and downs, like the infamous \"Motu Rona Divas\" incident, a reminder that even the strongest bonds can weather storms. But through it all, our understanding and communication have been our anchors. sometimes not lol, but that's life, we grow, right?"
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b76f9aaa7155a0fb9c_WhatsApp%20Image%202024-03-03%20at%208.30.31%20PM%20(2).jpeg",
+        "date": "February 7th, 2024",
+        "message": "You make me want to be a better man, Motu. Your intelligence and drive inspire me, and your love is a constant source of strength. I know I need to work on my own insecurities and respect your space, your time, your dreams."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4e87a42a5e623f85303d7_WhatsApp%20Image%202024-03-03%20at%2010.14.58%20PM%20(1).jpeg",
+        "date": "February 8th, 2024",
+        "message": "Your silence speaks volumes, Motu. Just like you, I know when the emojis disappear, something's off. My love, remember, you don't have to carry the weight of the world alone. Talk to me, let me share your burdens, and allow me to love you through anything."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b86f9aaa7155a0fbc4_WhatsApp%20Image%202024-03-03%20at%208.30.28%20PM.jpeg",
+        "date": "February 9th, 2024",
+        "message": "Motu, my Puppala Sri Esha, your birthday countdown continues and with it, my heart swells with the joy of celebrating you. Today, I remember the warmth of your laughter, the kind that can fill a room and chase away any darkness. You have a gift for making the ordinary extraordinary, and being loved by you feels like a daily adventure."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b840f3a80050289b6f_WhatsApp%20Image%202024-03-03%20at%208.30.31%20PM.jpeg",
+        "date": "February 10th, 2024",
+        "message": "Those nights where we say \"I love you\" and goodnight are the moments I hold dear, a silent promise whispered across the miles that binds us together. You may be miles away, but your presence is a constant comfort, a warm sunbeam even in the coldest night."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b865febd941c350a17_WhatsApp%20Image%202024-03-03%20at%208.30.17%20PM%20(1).jpeg",
+        "date": "February 11th, 2024",
+        "message": "I remember the days dinni you \"went crazy for the great Dingu,\" not because of the fights, but because they led to the beautiful chaos that is us. We may not be perfect, but our imperfections somehow fit together, creating a unique and precious puzzle of love."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b876da45aae72ef5c7_WhatsApp%20Image%202024-03-03%20at%208.30.19%20PM.jpeg",
+        "date": "February 12th, 2024",
+        "message": "Looking forward to our future together, Motu. The virtual world we share has been a beautiful playground, but I can't wait for the day our \"Motu-Muffin\" becomes reality, a day filled with first kisses, bayside dates, and adventures hand-in-hand."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b827c5a483d0d974ea_WhatsApp%20Image%202024-03-03%20at%208.30.22%20PM%20(1).jpeg",
+        "date": "February 13th, 2024",
+        "message": "Happy (almost) birthday, Motu! You are the gold in my \"Gol Gappa,\" the sunshine in my \"Motu,\" the love that makes my world complete. As you turn 22, may it be a year filled with laughter, joy, and the fulfillment of all your dreams(formal wishes lol). And remember, no matter the distance, my love for you remains constant, a guiding star leading me closer to you with each passing day."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4e87a2742f182a72271b9_WhatsApp%20Image%202024-03-03%20at%2010.14.58%20PM.jpeg",
+        "date": "February 14th, 2024",
+        "message": "Happy Valentine's Day, my motu! Though miles separate us, today feels even more special knowing we share this love together. Remember, you're not just my girlfriend, you're my confidante, my best friend, the one who makes my heart skip a beat with every thoughtful message and every goofy emoji."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4e87a801d8f6ceab5bb1b_WhatsApp%20Image%202024-03-03%20at%2010.14.57%20PM.jpeg",
+        "date": "February 15th, 2024",
+        "message": "I find myself constantly amazed by your strength, motu. You juggle your studies, your family, and our love with such grace and determination. You inspire me to be a better version of myself, to dream bigger and reach for the stars, just like you do."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4e87a0caabe7e9879c895_WhatsApp%20Image%202024-03-03%20at%2010.14.59%20PM.jpeg",
+        "date": "February 16th, 2024",
+        "message": "They say absence makes the heart grow fonder, and motu, my heart overflows with every passing day. Counting down the days until I can see your beautiful smile in person, hear your infectious laughter, and get lost in the depths of your incredible eyes."
+    },
+
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8140209a7c284da97_WhatsApp%20Image%202024-03-03%20at%208.30.20%20PM.jpeg",
+        "date": "February 17th, 2024",
+        "message": "Remember that promise we made, never to leave each other, never to let the other cry alone? It's a promise I hold close to my heart, a constant reminder of the unbreakable bond we share. No matter what life throws our way, we'll face it together, hand in hand, our love a guiding light through any storm."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b89c78c7ed6f53c882_WhatsApp%20Image%202024-03-03%20at%208.30.17%20PM.jpeg",
+        "date": "February 18th, 2024",
+        "message": "Two weeks until your birthday, Motu! With each passing day, the excitement builds, the anticipation grows, just like the love I have for you. You are the sunshine that brightens my day, the moon that lights my path, and the melody that fills my heart with joy. Happy (almost) birthday, my love!"
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8540d28184bdc3a0d_WhatsApp%20Image%202024-03-03%20at%208.30.16%20PM.jpeg",
+        "date": "February 19th, 2024",
+        "message": "Motu, my love, do you remember the day Motulotu and Mutki(the 14th that passed?) were born? A symbol of our playful connection, a testament to the joy we bring into each other's lives. These past few years have been filled with laughter, love, and a touch of goofiness, a testament to the unique bond we share."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8540d28184bdc3a1d_WhatsApp%20Image%202024-03-03%20at%208.30.16%20PM%20(1).jpeg",
+        "date": "February 20th, 2024",
+        "message": "I can't wait to see your face light up when you finally read all these entries, Motu. It's a small way for me to express the depth of my love for you, the emotions that often get lost in translation through texts and calls. You are my sunshine, my muse, my forever adventure buddy."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8ff94b5ce5de9fcb6_WhatsApp%20Image%202024-03-03%20at%208.30.23%20PM.jpeg",
+        "date": "February 21st, 2024",
+        "message": "Today, I want to celebrate your intelligence, Motu. Your sharp mind and your unwavering dedication to your dreams inspire me every day. You're not afraid to challenge the status quo, and your love is truly infectious. Seeing you motivates me to push myself further, reminding me that together, we can achieve anything."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8b3f249496972a439_WhatsApp%20Image%202024-03-03%20at%208.30.21%20PM%20(1).jpeg",
+        "date": "February 22nd, 2024",
+        "message": "One week until your birthday, Motu! The excitement is palpable, like a vibrant butterfly fluttering in my stomach. I can't wait to finally shower you with the love and appreciation you deserve, even if it's still through a screen for now."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b86489506ec3588776_WhatsApp%20Image%202024-03-03%20at%208.30.26%20PM%20(1).jpeg",
+        "date": "February 23rd, 2024",
+        "message": "Remember our promise of transparency and space, Motu? It might seem paradoxical, but it's the foundation of our trust. Knowing you feel comfortable enough to share your joys and sorrows, your triumphs and struggles, is what makes our connection so special. I promise to always listen with an open heart and create a safe space for you to be your true self."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b89c78c7ed6f53c85e_WhatsApp%20Image%202024-03-03%20at%208.30.25%20PM.jpeg",
+        "date": "February 24th, 2024",
+        "message": "Remember that magical day, September 26th, 2021? Like a scene from a movie, our paths crossed, and a spark ignited. Who knew a simple hello could blossom into something so beautiful, so profound?"
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8461fac0e1a593366_WhatsApp%20Image%202024-03-03%20at%208.30.19%20PM%20(1).jpeg",
+        "date": "February 25th, 2024",
+        "message": "Fast forward to November 9th, 2021, a date etched in our hearts. We took the leap, making our love official, a day that marked the beginning of our incredible journey together."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8ad9fdf0f3f85ce04_WhatsApp%20Image%202024-03-03%20at%208.30.21%20PM.jpeg",
+        "date": "February 26th, 2024",
+        "message": "Our path wasn't always smooth, Motu. We've had our disagreements, tiny storms that tested the strength of our bond. Remember \"Motu Rona Divas\"? A silly fight, but a reminder that even the strongest connections need a little TLC."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b885eee5aa369ec420_WhatsApp%20Image%202024-03-03%20at%208.30.26%20PM.jpeg",
+        "date": "February 27th, 2024",
+        "message": "Through it all, communication and understanding became our guiding lights(yes yes sometimes we both suck at it). We learned to listen, to empathize, and most importantly, to love each other through thick and thin."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8618f61dc86e87522_WhatsApp%20Image%202024-03-03%20at%208.30.27%20PM%20(1).jpeg",
+        "date": "February 28th, 2024",
+        "message": "Six days until your birthday, Motu! Six days until we celebrate the incredible woman you are, the love that brightens my world. This is just a glimpse into our story, a love that continues to grow stronger with each passing day. Stay tuned, my love, for more adventures to come..."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8ad9fdf0f3f85ce08_WhatsApp%20Image%202024-03-03%20at%208.30.29%20PM.jpeg",
+        "date": "February 29th, 2024",
+        "message": "Remember those late-night fights, Motu, fueled by misunderstandings and the frustration of distance? We'd dissect every word, every emoji, searching for hidden meanings, only to realize the solution often lay in a simple conversation, a shared silence, or a goofy meme that brought us back to laughter."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8936d2887a03de8bc_WhatsApp%20Image%202024-03-03%20at%208.30.27%20PM.jpeg",
+        "date": "March 1st, 2024",
+        "message": "Those fights, though sometimes hurtful, became stepping stones to a deeper understanding. We learned the language of each other's hearts, the unspoken cues that bridge the physical gap. We discovered the power of vulnerability, of expressing our fears and anxieties, knowing we would be met with love and acceptance."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b8f75f133236268468_WhatsApp%20Image%202024-03-03%20at%208.30.32%20PM.jpeg",
+        "date": "March 2nd, 2024",
+        "message": "Today, as we stand on the threshold of your 22nd birthday, Motu, our love story is far from over. It's a testament to the strength we've built together, brick by emotional brick. We've weathered storms, celebrated victories, and grown together in ways we never imagined."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4d5b89c78c7ed6f53c882_WhatsApp%20Image%202024-03-03%20at%208.30.17%20PM.jpeg",
+        "date": "March 3rd, 2024",
+        "message": "Tomorrow, Motu, the world celebrates you. But for me, every day is a celebration of your existence, your love, and your unwavering spirit. As you turn 22, I see a future filled with vibrant colors, shared dreams, and adventures yet to be embarked upon."
+      },
+      {
+        "image": "https://uploads-ssl.webflow.com/65e4b51d34d216f5a1a63fb0/65e4de239743dee8dd078c9b_pic1.png",
+        "date": "March 4th, 2024",
+        "message": "Happy Birthday, Motu! Today, as you blow out the candles, make a wish for a future as bright as your smile, as beautiful as your eyes, and as strong as the love we share. Though miles may separate us now, know that my heart beats only for you. You are the sun in my sky, the moon in my night, and the melody that fills my world with endless love."
+      }
+
+    ]
+  }
+  console.log(birthday_journals.days[0].message);
+
+  const container = document.getElementById("additems");
+function addItems(id){
+    const newElement = document.createElement("div");
+    newElement.setAttribute("data-w-id", "2646e7a8-ee8e-faed-3d40-f00747296a5b");
+    newElement.setAttribute("role", "listitem");
+    newElement.classList.add("main-grid", "w-dyn-item");
+    // Create and configure thumbnail element
+  const thumbnail = document.createElement("div");
+  thumbnail.classList.add("thumbnail-module");
+  
+  const img = document.createElement("img");
+  img.src = birthday_journals.days[id].image;
+  img.alt = "";
+  img.classList.add("thumbnail-image");
+  
+  thumbnail.appendChild(img);
+  
+  // Create and configure content element
+  const content = document.createElement("div");
+  content.classList.add("content-module");
+  
+  const articleContent = document.createElement("div");
+  articleContent.classList.add("article-content-wrapper");
+  
+  const richText = document.createElement("div");
+  richText.setAttribute("data-w-id", "83c2009b-fc37-2eda-f1b9-c4085be98894");
+  richText.classList.add("rich-text", "w-richtext");
+  richText.style.opacity = 100;
+  
+  const h3 = document.createElement("h3");
+  h3.textContent = birthday_journals.days[id].date;
+
+  const p = document.createElement("p");
+  p.textContent = "Day "+(id+1);
+  
+  const blockquote = document.createElement("blockquote");
+  blockquote.textContent = birthday_journals.days[id].message;
+  richText.appendChild(h3);
+  richText.appendChild(p);
+  richText.appendChild(blockquote);
+  
+  articleContent.appendChild(richText);
+  content.appendChild(articleContent);
+  
+  // Add child elements to the main element
+  newElement.appendChild(thumbnail);
+  newElement.appendChild(content);
+  container.appendChild(newElement);
+}
+
+const password = "motulotu2024";
+if (prompt("This is the journals for Dinni's birthday in 2024, you need a password for this file:") === password){for (let i = 0; i < 30; i++) {
+    addItems(i);
+}
+}
